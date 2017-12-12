fdcecca105b7c180709e9c02ec115020b97e11cc
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:08:23 +0100

    fini

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 5</title>
+  </head>
+  <body>
+    <?php
+      $pas=1;
+      for($pas=1;$pas <=15;$pas++){
+        if($pas < 15){
+          echo 'On y arrive presque.';
+        }else{
+          echo 'On y arrive.';
+        }
+      }
+     ?>
+  </body>
+</html>
